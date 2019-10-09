9e030f1c139cf6c527f0ae1e999d62cde3920764
yaplans <yaplans@mail.ru>
2019-10-10 00:02:38 +0600

    my1

---
+++ lk.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" 
+	href="css/bootstrap.min.css"
+	 >
+	<link rel="stylesheet" href="css/lk.css">
+</head>
+<body>
+	
+<div class="conteiner">
+	<div class="myb col-sm-2">Lore</div>
+	<div class="myb col-sm-2">Lor*20</div>
+	<div class="myb col-sm-2">
+		<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse earum, cumque enim quam. Earum eius reiciendis repellat harum, voluptas atque labore ratione assumenda quasi voluptate, rem esse, saepe officiis inventore!</div>
+      </div>
+	<div class="myb col-sm-2">Lorem*20</div>
+	<div class="myb col-sm-2">sdflksdfskl sfdsd</div>
+	<div class="myb col-sm-2">dsfd</div>
+	<div class="myb col-sm-2">erewrwr</div>
+	<div class="myb col-sm-2">xcvxvx</div>
+	<div class="myb col-sm-2">433535</div>
+	<div class="myb col-sm-2">3534543</div>
+
+</div>
+
+</body>
+</html>
