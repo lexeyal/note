3d9414d3fdba56ed938b6957d7f2ec07a9835442
yaplans <yaplans@mail.ru>
2019-10-11 00:08:41 +0600

    css

--- lk.php
+++ lk.php
@@ -6,23 +6,34 @@
 	<link rel="stylesheet" 
 	href="css/bootstrap.min.css"
 	 >
-	<link rel="stylesheet" href="css/lk.css">
+	<link rel="stylesheet" href="lk.css">
 </head>
 <body>
+	<?php
+
+	// xs  min-
+	// sm  min_768
+	// md  min-992
+	// lg  min-1299
+
+
+
+	$m_class="col-sm-6 col-lg-1 col-md-3 col-xs-12";
+	?>
 	
 <div class="conteiner">
-	<div class="myb col-sm-2">Lore</div>
-	<div class="myb col-sm-2">Lor*20</div>
-	<div class="myb col-sm-2">
+	<div class="myb <?php echo $m_class;?>">Lore</div>
+	<div class="myb <?php echo $m_class;?>">Lor*20</div>
+	<div class="myb <?php echo $m_class;?>">
 		<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse earum, cumque enim quam. Earum eius reiciendis repellat harum, voluptas atque labore ratione assumenda quasi voluptate, rem esse, saepe officiis inventore!</div>
       </div>
-	<div class="myb col-sm-2">Lorem*20</div>
-	<div class="myb col-sm-2">sdflksdfskl sfdsd</div>
-	<div class="myb col-sm-2">dsfd</div>
-	<div class="myb col-sm-2">erewrwr</div>
-	<div class="myb col-sm-2">xcvxvx</div>
-	<div class="myb col-sm-2">433535</div>
-	<div class="myb col-sm-2">3534543</div>
+	<div class="myb <?php echo $m_class;?>">Lorem*20</div>
+	<div class="myb <?php echo $m_class;?>">sdflksdfskl sfdsd</div>
+	<div class="myb <?php echo $m_class;?>">dsfd</div>
+	<div class="myb <?php echo $m_class;?>">erewrwr</div>
+	<div class="myb <?php echo $m_class;?>">xcvxvx</div>
+	<div class="myb <?php echo $m_class;?>">433535</div>
+	<div class="myb <?php echo $m_class;?>">3534543</div>
 
 </div>
 
